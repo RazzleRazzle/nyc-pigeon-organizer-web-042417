2d6776690c239d33d5759953c13d16d4250f882c
Jake Musich <makejusich@gmail.com>
2017-04-22 04:51:25 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+
+  data.each do |keys_1, data_1|
+    data_1.each do |keys_2, data_2|
+      data_2.each do |name|
+        pigeon_list[name] ||= {}
+        pigeon_list[name][keys_1] ||= []
+        pigeon_list[name][keys_1] << keys_2.to_s
+      end
+    end
+  end
+  pigeon_list 
 end
